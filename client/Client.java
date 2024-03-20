4853a5890a1c5058d861bcf4d86d2c38ae25aa86
Lucas Guilherme Tascheck <lucasguitascheck@gmail.com>
2024-03-20 18:32:32 -0300

    feat: Client impl

--- client/Client.java
+++ client/Client.java
@@ -1,16 +1,71 @@
 package client;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 public class Client {
     
 
     public static void main(String[] args) {
-        // Verifica args (IP e porta do server)
-        // Tenta conexao 
-        // Pede senha por teclado
-        // autentica ou nao no server
-        // Quando conectar, recebe IP do multicast (por enquanto, só printa multicast)
-        // Ler teclado esperando msg
-        // Enviar msg por tcp
+        int portNumber = 2222;
+        String serverIP = "";
+        if (args.length >= 2) {
+            serverIP = args[0];
+            portNumber = Integer.valueOf(args[1]).intValue();
+
+            try {
+                Socket clientSocket = new Socket(serverIP, portNumber);
+    
+                String message;
+                String response = "";
+                String multicastIP;
+    
+                BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+                BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+
+                //aguarda por resposta
+                while(response == ""){
+                    response = inFromServer.readLine();
+                }
+                System.out.println(response);
+
+                //Manda senha
+                message = inFromUser.readLine();
+                outToServer.writeBytes(message + '\n');
+
+                //Recebe confirmação
+                multicastIP = inFromServer.readLine();
+                if(multicastIP == "Senha errada."){
+                    System.out.println("Senha incorreta");
+                    clientSocket.close();
+                }
+
+                //Nao sei se essa parte a seguir deve ser implementada desse modo:
+                
+                // Multicast multicast = new Multicast(multicastIP);
+                // multicast.startConnection();
+
+                // while(true){
+                //     multicast.getMessage();
+                //     message = inFromUser.readLine();
+                //     outToServer.writeBytes(message + '\n');
+                //     if(message == "/quit"){
+                //         break;
+                //     }
+                // }
+                clientSocket.close();
+                
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+
+
+        }else{
+            System.out.println("porta ou ip nao informados!");
+        }
     }
 
     public String getMulticastIp() {
