e568027ad1e0055aa667439cb292fa5d434eee92
GabrielFJunkes <gabriel.f.junkes@gmail.com>
2024-03-30 20:29:34 -0300

    fix: comma

--- 02-rmi/Client.java
+++ 02-rmi/Client.java
@@ -5,59 +5,57 @@
 import java.rmi.registry.*;
 
 public class Client {
- public static void main(String[] args) {
-      if(args.length >= 2){
-        String serverMatrixIP = args[0];
-        String serverDatabaseIP = args[1];
-        try {
-            int linhas = 3;
-            int colunas = 3;
-            String diretorioSumBackup = "sumBackup.txt";
-            String diretorioMultBackup = "multBackup.txt";
+  public static void main(String[] args) {
+    if (args.length >= 2) {
+      String serverMatrixIP = args[0];
+      String serverDatabaseIP = args[1];
+      try {
+        int linhas = 3;
+        int colunas = 3;
+        String diretorioSumBackup = "sumBackup.txt";
+        String diretorioMultBackup = "multBackup.txt";
 
-            double[][] matrizA = new double[linhas][colunas];
-            double[][] matrizB = new double[linhas][colunas];
+        double[][] matrizA = new double[linhas][colunas];
+        double[][] matrizB = new double[linhas][colunas];
 
-            double[][] sum = new double[linhas][colunas];
-            double[][] mult = new double[linhas][colunas];
+        double[][] sum = new double[linhas][colunas];
+        double[][] mult = new double[linhas][colunas];
 
+        // Obtém uma referência para o registro do RMI
+        Registry serverMatrixRegistry = LocateRegistry.getRegistry(serverMatrixIP, 6600);
+        Registry serverDatabaseRegistry = LocateRegistry.getRegistry(serverDatabaseIP, 6601);
 
-            // Obtém uma referência para o registro do RMI
-            Registry serverMatrixRegistry = LocateRegistry.getRegistry(serverMatrixIP,6600);
-            Registry serverDatabaseRegistry = LocateRegistry.getRegistry(serverDatabaseIP, 6601);
+        // Obtém a stub do servidor
+        IMatrix serverMatrixStub = (IMatrix) serverMatrixRegistry.lookup("Matrix");
+        IDatabase serverDatabaseStub = (IDatabase) serverDatabaseRegistry.lookup("Database");
 
-            // Obtém a stub do servidor
-            IMatrix serverMatrixStub= (IMatrix) serverMatrixRegistry.lookup("Matrix");
-            IDatabase serverDatabaseStub = (IDatabase) serverDatabaseRegistry.lookup("Database");
+        // Chama o método do servidor para inicializar as matrizes
+        matrizA = serverMatrixStub.randfill(linhas, colunas);
+        matrizB = serverMatrixStub.randfill(linhas, colunas);
+        System.out.println("Matriz A inicial: " + matrizA);
+        System.out.println("Matriz B inicial: " + matrizB);
 
-            // Chama o método do servidor para inicializar as matrizes
-            matrizA = serverMatrixStub.randfill(linhas, colunas);
-            matrizB = serverMatrixStub.randfill(linhas, colunas);
-            System.out.println("Matriz A inicial: ", matrizA);
-            System.out.println("Matriz B inicial: ", matrizB);
+        // Chama o método do servidor para somar as matrizes e salva
+        serverDatabaseStub.save(serverMatrixStub.sum(matrizA, matrizB), diretorioSumBackup);
 
-            // Chama o método do servidor para somar as matrizes e salva
-            serverDatabaseStub.save(serverMatrixStub.sum(matrizA, matrizB), diretorioSumBackup);
+        // Chama o método do servidor para multiplicar as matrizes e salva
+        serverDatabaseStub.save(serverMatrixStub.mult(matrizA, matrizB), diretorioMultBackup);
 
-            // Chama o método do servidor para multiplicar as matrizes e salva
-            serverDatabaseStub.save(serverMatrixStub.mult(matrizA, matrizB), diretorioMultBackup);
+        // recupera as matrizes e remove o arquivo de backup
+        sum = serverDatabaseStub.load(diretorioSumBackup);
+        serverDatabaseStub.remove(diretorioSumBackup);
+        System.out.println("Resultado da soma das matrizes: " + sum);
 
-            //recupera as matrizes e remove o arquivo de backup
-            sum = serverDatabaseStub.load(diretorioSumBackup);
-            serverDatabaseStub.remove(diretorioSumBackup);
-            System.out.println("Resultado da soma das matrizes: ", sum);
+        mult = serverDatabaseStub.load(diretorioMultBackup);
+        serverDatabaseStub.remove(diretorioMultBackup);
+        System.out.println("Resultado da multiplicacao das matrizes: " + mult);
 
-            mult = serverDatabaseStub.load(diretorioMultBackup);
-            serverDatabaseStub.remove(diretorioMultBackup);
-            System.out.println("Resultado da multiplicacao das matrizes: ", mult);
-
-
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-
-      }else{
-        System.out.println("IP ServerMatrix e/ou IP SeverDatabase faltando");
+      } catch (Exception ex) {
+        ex.printStackTrace();
       }
-   }   
+
+    } else {
+      System.out.println("IP ServerMatrix e/ou IP SeverDatabase faltando");
+    }
+  }
 }
