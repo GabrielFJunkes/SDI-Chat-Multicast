3d61b0a8565d44eec6c08c6370aeaee140a5eeab
GabrielFJunkes <gabriel.f.junkes@gmail.com>
2024-03-30 21:11:54 -0300

    fix: proper print matrix

--- 02-rmi/Client.java
+++ 02-rmi/Client.java
@@ -32,8 +32,10 @@ public class Client {
         // Chama o método do servidor para inicializar as matrizes
         matrizA = serverMatrixStub.randfill(linhas, colunas);
         matrizB = serverMatrixStub.randfill(linhas, colunas);
-        System.out.println("Matriz A inicial: " + matrizA);
-        System.out.println("Matriz B inicial: " + matrizB);
+        System.out.println("Matriz A inicial: ");
+        print(matrizA);
+        System.out.println("Matriz B inicial: ");
+        print(matrizB);
 
         // Chama o método do servidor para somar as matrizes e salva
         serverDatabaseStub.save(serverMatrixStub.sum(matrizA, matrizB), diretorioSumBackup);
@@ -44,11 +46,13 @@ public class Client {
         // recupera as matrizes e remove o arquivo de backup
         sum = serverDatabaseStub.load(diretorioSumBackup);
         serverDatabaseStub.remove(diretorioSumBackup);
-        System.out.println("Resultado da soma das matrizes: " + sum);
+        System.out.println("Resultado da soma das matrizes: ");
+        print(sum);
 
         mult = serverDatabaseStub.load(diretorioMultBackup);
         serverDatabaseStub.remove(diretorioMultBackup);
-        System.out.println("Resultado da multiplicacao das matrizes: " + mult);
+        System.out.println("Resultado da multiplicacao das matrizes: ");
+        print(mult);
 
       } catch (Exception ex) {
         ex.printStackTrace();
@@ -56,6 +60,17 @@ public class Client {
 
     } else {
       System.out.println("IP ServerMatrix e/ou IP SeverDatabase faltando");
+    }
+  }
+
+  private static void print(double[][] matrix) {
+    int rowsA = matrix.length;
+    int colsA = matrix[0].length;
+    for (int i = 0; i < rowsA; i++) {
+      for (int j = 0; j < colsA; j++) {
+        System.out.print(matrix[i][j] + "\t");
+      }
+      System.out.println(" ");
     }
   }
 }
