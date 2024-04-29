2cca2c116fb7f56d9e7e75be659daddf41d62a9b
Lucas-Tascheck <lucasguitascheck@gmail.com>
2024-04-29 20:25:03 -0300

    feat: client impl

--- 03-filas/client/Client.java
+++ 03-filas/client/Client.java
@@ -1,9 +1,35 @@
 package client;
+import utils.Message
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
 
 public class Client {
+    private final static String LOW_PRIOR_QUEUE = "filinha"
+    private final static String HIGH_PRIOR_QUEUE = "filhona"
+
     public static void main(String[] argv) throws Exception {
-        // Tempo de 1 a 20
-        // Recursos de 1 a 10
-        // Prioridade alta ou baixa
+        String numberOfMsg = args[0];
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(LOW_PRIOR_QUEUE, false, false, false, null);
+        channel.queueDeclare(HIGH_PRIOR_QUEUE, false, false, false, null);
+
+        for (int i = 0; i < numberOfMsg; i++) {
+
+            Message newMessage = new Message();
+
+            //Implementar getter de prioridades?
+            if(newMessage.getPriotity() == 0){
+                channel.basicPublish("", LOW_PRIOR_QUEUE, null, newMessage);
+            }
+            else {
+                channel.basicPublish("", HIGH_PRIOR_QUEUE, null, newMessage);
+            }
+        }
     }
 }
