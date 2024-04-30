08f18cc01b654f6dab0a972eabb49383fc995c7a
GabrielFJunkes <gabriel.f.junkes@gmail.com>
2024-04-29 22:00:59 -0300

    feat: log

---
+++ 03-filas/utils/Log.java
@@ -0,0 +1,29 @@
+package utils;
+
+public class Log {
+    private Message message;
+    private long receivedTime;
+    private long startedTime;
+
+    public Log(Message message) {
+        this.message = message;
+        this.receivedTime = System.currentTimeMillis();
+    }
+
+    public void startMessage() {
+        this.startedTime = System.currentTimeMillis();
+    }
+
+    public String toString() {
+        return 
+            this.message.getTime() 
+            + " " + 
+            this.message.getResource()
+            + " " + 
+            this.receivedTime
+            + " " + 
+            this.startedTime
+            + " " + 
+            (this.startedTime + this.message.getTime());
+    }
+}
---
+++ 03-filas/utils/Logger.java
@@ -0,0 +1,47 @@
+package utils;
+
+import java.io.FileOutputStream;
+import java.io.Writer;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.util.Random;
+import java.util.Vector;
+
+public class Logger {
+    private Vector<Log> logs;
+
+    public Logger() {
+        this.logs = new Vector<Log>();
+        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+            public void run() {
+                saveToFile();
+            }
+        }, "Shutdown-thread"));
+    }
+
+    /**
+     * @param message
+     * @return index for the added message
+     */
+    public int addMessage(Message message) {
+        Log log = new Log(message);
+        logs.add(log);
+        return logs.size();
+    }
+
+    public void saveToFile() {
+        Random rand = new Random();
+        try (Writer writer = new BufferedWriter(
+                new OutputStreamWriter(new FileOutputStream("logs/log-" + rand.nextLong() + ".txt"), "utf-8"))) {
+            for (Log log : logs) {
+                writer.write(log.toString());
+            }
+        } catch (Exception e) {
+            for (Log log : logs) {
+                System.out.println(log);
+            }
+        }
+
+    }
+
+}
