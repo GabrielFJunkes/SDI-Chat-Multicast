160abde28839b8c82390f2ac4f53735214fe5145
GabrielFJunkes <gabriel.f.junkes@gmail.com>
2024-05-02 20:46:58 -0300

    feat: temp server

--- 03-filas/server/Server.java
+++ 03-filas/server/Server.java
@@ -1,6 +1,12 @@
 package server;
 
+import java.io.IOException;
+
+import utils.Message;
+
 public class Server {
+    private final static String LOW_PRIOR_QUEUE = "filinha";
+    private final static String HIGH_PRIOR_QUEUE = "filhona";
     public static void main(String[] argv) throws Exception {
         /*
          * Objeto server
@@ -26,5 +32,23 @@ public class Server {
          * 
          * No fim do programa, criar arquivo com os logs
          */
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(LOW_PRIOR_QUEUE, false, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties,
+                    byte[] body)
+                    throws IOException {
+                Message message = Message.fromBytes(body) ;
+                System.out.println(" [x] Received '" + message + "'");
+            }
+        };
+        channel.basicConsume(LOW_PRIOR_QUEUE, true, consumer);
     }
 }
--- 03-filas/utils/Message.java
+++ 03-filas/utils/Message.java
@@ -1,12 +1,14 @@
 package utils;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.Random;
 
-public class Message implements Serializable{
+public class Message implements Serializable {
     private final int time;
     private final int resource;
 
@@ -32,12 +34,20 @@ public class Message implements Serializable{
     public int getTime() {
         return time;
     }
-    
+
     public byte[] toBytes() throws IOException {
         try (ByteArrayOutputStream bos = new ByteArrayOutputStream();
                 ObjectOutputStream oos = new ObjectOutputStream(bos)) {
             oos.writeObject(this);
             return bos.toByteArray();
+        }
+    }
+
+    public static Message fromBytes(byte[] bytes) throws IOException, ClassNotFoundException {
+        try (ByteArrayInputStream bis = new ByteArrayInputStream(
+                bytes);
+                ObjectInputStream ois = new ObjectInputStream(bis)) {
+            return (Message) ois.readObject();
         }
     }
 }
