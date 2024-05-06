45ae8480d06d5ede80ac7f6db4fd26cf4eb6a7ba
GabrielFJunkes <gabriel.f.junkes@gmail.com>
2024-05-05 21:27:45 -0300

    fix: client

--- 03-filas/client/Client.java
+++ 03-filas/client/Client.java
@@ -26,10 +26,10 @@ public class Client {
             Message newMessage = new Message();
 
             if(rand.nextInt(2) == 0){
-                channel.basicPublish("", LOW_PRIOR_QUEUE, null, newMessage.toBytes());
+                channel.basicPublish("", Common.LOW_PRIOR_QUEUE, null, newMessage.toBytes());
             }
             else {
-                channel2.basicPublish("", HIGH_PRIOR_QUEUE, null, newMessage.toBytes());
+                channel2.basicPublish("", Common.HIGH_PRIOR_QUEUE, null, newMessage.toBytes());
             }
         }
     }
